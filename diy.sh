8439110adc32aa8c6cb979088204c3453f1721d0
qhq <qhqcz@qq.com>
2021-06-24 00:43:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -284,7 +284,8 @@ echo -e "+--------------------------------------------+\n"
 
 
 echo -e "+---------------- star261 脚本 ---------------+"
-perl -0777 -i -pe "s|https:\/\/raw\.githubusercontent\.com\/star261\/jd\/main\/code\/dreamFactory_tuan\.json|http://xinhunshang.xyz:6001/submit_activity_codes/get/jxfactorytuan/20/2|ig" ${ScriptsDir}/star261_star_dreamFactory_tuan.js >/dev/null 2>&1 && echo -e " 京喜工厂库链接已替换"
+#perl -0777 -i -pe "s|https:\/\/raw\.githubusercontent\.com\/star261\/jd\/main\/code\/dreamFactory_tuan\.json|http://xinhunshang.xyz:6001/submit_activity_codes/get/jxfactorytuan/20/2|ig" ${ScriptsDir}/star261_star_dreamFactory_tuan.js >/dev/null 2>&1 && echo -e " 京喜工厂库链接已替换"
+sed -i "/res = await getAuthorShareCode/a await $.getScript('http://xinhunshang.xyz:6001/submit_activity_codes/get/jxfactorytuan/20/2').then((text) => (res = JSON.parse(text).data))" ${ScriptsDir}/star261_star_dreamFactory_tuan.js >/dev/null 2>&1 && echo -e " 京喜工厂库链接已替换"
 sed -i "/开团成功tuanId为/a $.getScript\(\`http://xinhunshang.xyz:6001/submit_activity_codes/jxfactorytuan/$\{data.data['tuanId']\}/$\{$.UserName\}\`\).then\(\(text\) => \(console.log\(text\)\)\);" ${ScriptsDir}/star261_star_dreamFactory_tuan.js >/dev/null 2>&1 && echo -e " 京喜工厂库链接已替换"
 
 echo -e "+--------------------------------------------+\n"
