c501f99da527017295a43cb30c120bdfd7969b0f
qhq <qhqcz@qq.com>
2021-06-29 19:04:35 +0800

    Update server.js

--- Scripts/server.js
+++ Scripts/server.js
@@ -923,7 +923,7 @@ app.post('/updateCookie', function (request, response) {
             }
         }
         let CookieCount = Number(maxCookieCount) + 1;
-        if (!updateFlag && CK_ATUO_ADD = 'true') {
+        if (!updateFlag && CK_ATUO_ADD == 'true') {
             let newLine = [
                 'Cookie',
                 CookieCount,
@@ -944,7 +944,7 @@ app.post('/updateCookie', function (request, response) {
         response.send({
             err: 0,
             msg: updateFlag ?
-                `[更新成功]\n当前用户量:(${maxCookieCount})` : CK_ATUO_ADD ? `[新的Cookie]\n当前用户量:(${CookieCount})` : `非本服用户\n本服用户量:(${CookieCount})`,
+                `[更新成功]\n当前用户量:(${maxCookieCount})${CK_ATUO_ADD}` : CK_ATUO_ADD ? `[新的Cookie]\n当前用户量:(${CookieCount})${CK_ATUO_ADD}` : `非本服用户\n本服用户量:(${CookieCount})${CK_ATUO_ADD}`,
                 //`[更新成功]\n本服用户量:(${maxCookieCount})` : `非本服用户\n本服用户量:(${CookieCount})`,
         });
     } else {
