2bb2cd0c298906c5c225643d19c568b821535a73
qhq <qhqcz@qq.com>
2021-06-30 21:51:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -26,7 +26,7 @@ echo -e "2021-06-30 21:35\n"
 JsList=$(grep -Eo "bash jd \w+" ${ConfigDir}/crontab.list)
 for Cron in ${JsList}; do
 	echo -e "${ScriptsDir}/${Cron##* }.js"
-	if [ -d ${ScriptsDir}/${Cron##* }.js ]; then
+	if [ -e ${ScriptsDir}/${Cron##* }.js ]; then
 		echo -e "GG"
     		#perl -i -ne "{print unless / ${Cron}( |$)/}" ${ListCron}
 	fi
