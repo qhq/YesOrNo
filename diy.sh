18eb1019dd1cea0affbe86ca5a19ce47ab1499e0
qhq <qhqcz@qq.com>
2021-07-16 09:44:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -450,7 +450,7 @@ echo -e "+--------------------------------------------+\n"
 
 ## 注释指定活动
 echo -e "+--------------- 暂时停用脚本 ---------------+"
-js_List="jd_bean_change qhqcz_jd_enen passerby_jd_fruit2 passerby_jd_dreamFactory2 jd_big_winner jd_star_shop smiek2221_jd_summer_movement_help"
+js_List="jd_bean_change qhqcz_jd_enen passerby_jd_fruit2 passerby_jd_dreamFactory2 jd_big_winner jd_star_shop smiek2221_jd_summer_movement_help jd_speed_redEnvelope"
 for js_item in $js_List; do
     sed -i "s|\(^[0-9].*bash\) jd $js_item|# \1 jd $js_item|" ${ListCron} && echo -e " 已注释 $js_item"
     #sed -i "/$js_item/d" ${ListCron} && echo -e "$js_item已删除"
