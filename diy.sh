167b7b6fb073018b0564dca81646dc2ddfbdc426
qhq <qhqcz@qq.com>
2021-07-30 11:34:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e " 2021-07-30 11:00"
+echo -e " 2021-07-30 10:00"
 
 sed -i 's|\(0xa\|0x23\)|40|g' $ShellDir/jd.sh && echo -e " 不解释"
 
@@ -457,7 +457,7 @@ echo -e "+--------------------------------------------+\n"
 #fi
 
 ## 修正定时
-grep -q "bash git_pull" ${ListCron} && sed -i 's|.*bash git_pull|7 1,7,13,19 \* \* \* bash git_pull|' ${ListCron}
+grep -q "bash git_pull" ${ListCron} && sed -i "/&*bash git_pull/c$(rand 1 59) 1,7,13,19 \* \* \* sleep $(rand 1 59) && bash git_pull >>\$\{JD_DIR\}\/log\/git_pull.log 2>&1" ${ListCron}
 
 ## bug fix
 #[ -f ${ScriptsDir}/main.3b9712aa.js ] && rm -rf ${ScriptsDir}/main.3b9712aa.js
