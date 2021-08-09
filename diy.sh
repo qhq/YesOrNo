ea251ec7edd78fa0957c522f1844a2446cd4ff94
qhq <qhqcz@qq.com>
2021-08-09 13:43:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e " 2021-08-08 16:00"
+echo -e " 2021-08-08 14:00"
 
 ############################## DIY更新状态检查 ##############################
 iCan=true
@@ -448,7 +448,7 @@ sed -i "s|\^export.*\?,|^export\\\s(qjd_zlzh)=[\\\'\\\\\"](.*?)[\\\'\\\\\"]{0,1}
 sed -i "/请重新登录获取cookie/d" ${ScriptsDir}/Tsukasa007_jd_joypark_task.js && echo -e " 汪汪乐园ck失效通知移除"
 [ $(grep -c "NOTIFY_SKIP_LIST" ${ScriptsDir}/sendNotify.js) -eq 0 ] && sed -i "/fs.accessSync/i\    const notifySkipList = process.env.NOTIFY_SKIP_LIST ? process.env.NOTIFY_SKIP_LIST.split('&') : [];\n    const titleIndex = notifySkipList.findIndex((item) => item === text);\n    if (titleIndex !== -1) {\n      console.log(\`\\n🔕🔕 ${text} 在推送黑名单中，已跳过推送 🔕🔕\\n\`);\n      return;\n    }"  ${ScriptsDir}/sendNotify.js >/dev/null 2>&1 && echo -e " 通知黑名单已添加"
 
-perl -0777 -i -pe "s|http:\/\/share\.turinglabs\.net\/api\/v3\/.*?\`,|http://xinhunshang.xyz:6001/help/v3/get/carnivalcity/5/1`|ig" ${ScriptsDir}/qhqcz_gua_carnivalcity.js >/dev/null 2>&1 && echo -e " 手机狂欢城库链接已替换"
+perl -0777 -i -pe "s|http:\/\/share\.turinglabs\.net\/api\/v3\/.*?\`|http://xinhunshang.xyz:6001/help/v3/get/carnivalcity/5/1\`|ig" ${ScriptsDir}/qhqcz_gua_carnivalcity.js >/dev/null 2>&1 && echo -e " 手机狂欢城库链接已替换"
 
 
 ## 验证调用
