6e77e99572688944417fbce8218dbd0e337e98c4
qhq <qhqcz@qq.com>
2021-08-15 23:10:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e " 2021-08-13 14:00"
+echo -e " 2021-08-14 14:00"
 
 ############################## DIY更新状态检查 ##############################
 iCan=true
@@ -127,7 +127,7 @@ my_scripts_list_Annyoo2021="jd_jxzpk.js"
 # Aaron-lv
 # https://github.com/Aaron-lv/sync/tree/jd_scripts
 scripts_base_url_Aaron_lv=${ProxyJudge}https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/
-my_scripts_list_Aaron_lv="jd_cfd.js jd_sendBeans.js jd_big_winner.js jd_cfdtx.js jd_necklace.js jd_joy_reward.js jd_joy_run.js jd_bean_sign.js jx_sign.js jd_mohe.js jd_carnivalcity_help.js"
+my_scripts_list_Aaron_lv="jd_cfd.js jd_sendBeans.js jd_big_winner.js jd_cfdtx.js jd_necklace.js jd_joy_reward.js jd_joy_run.js jd_bean_sign.js jx_sign.js jd_mohe.js jd_carnivalcity_help.js jd_jxmc.js"
 
 # shufflewzc
 # https://github.com/shufflewzc/faker2
