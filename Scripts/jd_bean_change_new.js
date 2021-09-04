8ea43496ae1c8ba86434945940e93ad2b74ef3da
qhq <qhqcz@qq.com>
2021-09-04 22:05:27 +0800

    Update jd_bean_change_new.js

--- Scripts/jd_bean_change_new.js
+++ Scripts/jd_bean_change_new.js
@@ -25,7 +25,7 @@ if ($.isNode()) {
 } else {
     cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
-cookiesArr=["pt_key=app_openAAJhMozTADDYuIAFbRfaJ--PLNCwZwdCHuT88WLH4raIXc1nDbheJMrMg2VkpsRj7IenATa3lho;pt_pin=%E7%8E%8B%E8%80%81%E8%99%8E%E6%8A%A2%E4%BA%B2;"]
+
 !(async () => {
     if (!cookiesArr[0]) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
