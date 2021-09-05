b8573c3e7e25ef9ea262c897e5237c503ab87e13
qhq <qhqcz@qq.com>
2021-09-05 23:12:54 +0800

    Update jd_cleancart.js

--- Scripts/jd_cleancart.js
+++ Scripts/jd_cleancart.js
@@ -9,12 +9,12 @@
 ==============Quantumult X===========
 [task_local]
 #清空购物车
-55 17 * * * jd_cleancart.js, tag=清空购物车, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+35 6-21/3 * * * jd_cleancart.js, tag=清空购物车, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 ===========Loon============
 [Script]
-cron "55 17 * * *" script-path=jd_cleancart.js,tag=清空购物车
+cron "35 6-21/3 * * *" script-path=jd_cleancart.js,tag=清空购物车
 ============Surge=============
-清空购物车 = type=cron,cronexp="55 17 * * *",wake-system=1,timeout=3600,script-path=jd_cleancart.js
+清空购物车 = type=cron,cronexp="35 6-21/3 * * *",wake-system=1,timeout=3600,script-path=jd_cleancart.js
 ===========小火箭========
 清空购物车 = type=cron,script-path=jd_cleancart.js, cronexpr="55 17 * * *", timeout=3600, enable=true
  */
